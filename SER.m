2e37e565d00703d9d9750adc12f3ac7a5e9914a0
Zicong Jiang <75415960+ZicongJiang@users.noreply.github.com>
2023-02-01 17:17:10 +0100

    Add files via upload

--- SER.m
+++ SER.m
@@ -1,20 +1,30 @@
-function symbol_error_rate = SER(format,N,loopnum,dB,input)
+function symbol_error_rate = SER(format,N,loopnum,dB,symbol_rate,samples,input)
 %UNTITLED 此处提供此函数的摘要
 %   此处提供详细说明
 snr=10.^(dB/10); % SNR in W
 switch format
     case 'OOK'
-        symbol_error_rate = OOK(N,loopnum,dB,snr);
+        symbol_error_rate = OOK(N,loopnum,dB,snr,symbol_rate,samples);
     case '2PAM'
-        symbol_error_rate = PAM_2(N,loopnum,snr,dB);
+        symbol_error_rate = PAM_2(N,loopnum,snr,dB,symbol_rate,samples);
     case '4PAM'
-        symbol_error_rate = PAM_4(N,loopnum,snr,dB);
+        symbol_error_rate = PAM_4(N,loopnum,snr,dB,symbol_rate,samples);
     case '4PAM_MZM'
-        symbol_error_rate = PAM_4_MZM(N,loopnum,snr);
+        symbol_error_rate = PAM_4_MZM(N,loopnum,snr,symbol_rate,samples);
     case '4PAM_MZM_with_arccos'
-        symbol_error_rate = PAM_4_MZM_arccos(N,loopnum,snr);
+        symbol_error_rate = PAM_4_MZM_arccos(N,loopnum,snr,symbol_rate,samples);
     case 'test_4PAM'
-        symbol_error_rate = test_4PAM(N,loopnum,snr,input);
+        symbol_error_rate = test_4PAM(N,loopnum,snr,input,symbol_rate,samples);
+
+    case 'OOK_ADC'
+        symbol_error_rate = OOK_ADC(N,loopnum,dB,snr,symbol_rate,samples);
+    case '4PAM_ADC'
+        symbol_error_rate = PAM_4_ADC(N,loopnum,snr,dB,symbol_rate,samples);
+
+    case '4PAM_MZM_ADC'
+        symbol_error_rate = PAM_4_MZM_ADC(N,loopnum,snr,symbol_rate,samples);
+    case '4PAM_MZM_with_ADC_arccos'
+        symbol_error_rate = PAM_4_MZM_ADC_arccos(N,loopnum,snr,symbol_rate,samples);
 
 end
 end
